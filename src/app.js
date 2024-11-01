9af6af0536f09ab2c7aceea68738a867a563ba83
Bogdan Litsenberger <bogdan.litsenberger@rebilly.com>
2024-11-01 11:58:09 +0500

    Update test BE

--- src/app.js
+++ src/app.js
@@ -6,171 +6,16 @@ const app = express();
 const port = 3000;
 app.use(cors());
 
-const REBILLY_API_SECRET_KEY = "sk_sandbox_i1a-6LXuIdnizfCXj5AjWUik1lMqRCGde9tXAT0";
-const REBILLY_WEBSITE_ID = "google.com";
-const REBILLY_ORGANIZATION_ID = 'phronesis---summitvilla';
+const REBILLY_API_SECRET_KEY = "sk_sandbox_bkJWBpH3Pfpl3QBJyjiaHn6tsaKJnJ9AxrO7Q40";
+const REBILLY_WEBSITE_ID = "rebilly.com";
+const REBILLY_ORGANIZATION_ID = 'phronesis-jackpot';
 const api = RebillyAPI({
     sandbox: true,
     apiKey: REBILLY_API_SECRET_KEY,
 });
 
-
-app.get('/invoices', (req, res) => {
-    api.invoices.getAll({filter:'customerId:cus_01J63Y0C1XZ5Q3M4G9W246KX2P'}).then(function (invoices){
-        res.send(invoices);
-    })
-});
-
-app.post('/deposit-request', (req, res) => {
-    const customerId = 'cus_01J72X65QDGA4DJ7FXSSXMRYRR';
-    const response = {};
-    const data = {
-        mode: "passwordless",
-        customerId,
-    };
-    api.customerAuthentication.login({
-        data,
-    }).then(function (login){
-        const { fields: exchangeToken } =
-            api.customerAuthentication.exchangeToken({
-                token: login.fields.token,
-                data: {
-                    acl: [
-                        {
-                            scope: {
-                                organizationId: [REBILLY_ORGANIZATION_ID],
-                            },
-                            permissions: [
-                                "PostToken",
-                                "PostDigitalWalletValidation",
-                                "StorefrontGetAccount",
-                                "StorefrontPatchAccount",
-                                "StorefrontPostPayment",
-                                "StorefrontGetTransactionCollection",
-                                "StorefrontGetTransaction",
-                                "StorefrontGetPaymentInstrumentCollection",
-                                "StorefrontPostPaymentInstrument",
-                                "StorefrontGetPaymentInstrument",
-                                "StorefrontPatchPaymentInstrument",
-                                "StorefrontPostPaymentInstrumentDeactivation",
-                                "StorefrontGetWebsite",
-                                "StorefrontGetInvoiceCollection",
-                                "StorefrontGetInvoice",
-                                "StorefrontGetProductCollection",
-                                "StorefrontGetProduct",
-                                "StorefrontPostReadyToPay",
-                                "StorefrontGetPaymentInstrumentSetup",
-                                "StorefrontPostPaymentInstrumentSetup",
-                                "StorefrontGetDepositRequest",
-                                "StorefrontGetDepositStrategy",
-                                "StorefrontPostDeposit",
-                            ],
-                        },
-                    ],
-                    customClaims: {
-                        websiteId: REBILLY_WEBSITE_ID,
-                    },
-                },
-            }).then(function (exchangeToken){
-                const requestDepositData = {
-                    websiteId: REBILLY_WEBSITE_ID,
-                    customerId,
-                    currency: "USD",
-                    amounts: [500*0.2, 500*0.3,500*0.5],
-                    "amountLimits": {
-                        "minimum": 500*0.2,
-                        "maximum": 500
-                    },
-                };
-                api.depositRequests.create({
-                    data: requestDepositData,
-                }).then(function (deposit){
-                    response.token = exchangeToken.fields.token;
-                    response.depositRequestId = deposit.fields.id;
-                    res.send(response);
-                });
-            });
-    });
-
-
-});
-
-app.post('/deposit-request-project-11', (req, res) => {
-    const customerId = 'cus_01J72X65QDGA4DJ7FXSSXMRYRR';
-    const response = {};
-    const data = {
-        mode: "passwordless",
-        customerId,
-    };
-    api.customerAuthentication.login({
-        data,
-    }).then(function (login){
-        const { fields: exchangeToken } =
-            api.customerAuthentication.exchangeToken({
-                token: login.fields.token,
-                data: {
-                    acl: [
-                        {
-                            scope: {
-                                organizationId: [REBILLY_ORGANIZATION_ID],
-                            },
-                            permissions: [
-                                "PostToken",
-                                "PostDigitalWalletValidation",
-                                "StorefrontGetAccount",
-                                "StorefrontPatchAccount",
-                                "StorefrontPostPayment",
-                                "StorefrontGetTransactionCollection",
-                                "StorefrontGetTransaction",
-                                "StorefrontGetPaymentInstrumentCollection",
-                                "StorefrontPostPaymentInstrument",
-                                "StorefrontGetPaymentInstrument",
-                                "StorefrontPatchPaymentInstrument",
-                                "StorefrontPostPaymentInstrumentDeactivation",
-                                "StorefrontGetWebsite",
-                                "StorefrontGetInvoiceCollection",
-                                "StorefrontGetInvoice",
-                                "StorefrontGetProductCollection",
-                                "StorefrontGetProduct",
-                                "StorefrontPostReadyToPay",
-                                "StorefrontGetPaymentInstrumentSetup",
-                                "StorefrontPostPaymentInstrumentSetup",
-                                "StorefrontGetDepositRequest",
-                                "StorefrontGetDepositStrategy",
-                                "StorefrontPostDeposit",
-                            ],
-                        },
-                    ],
-                    customClaims: {
-                        websiteId: REBILLY_WEBSITE_ID,
-                    },
-                },
-            }).then(function (exchangeToken){
-                const requestDepositData = {
-                    websiteId: REBILLY_WEBSITE_ID,
-                    customerId,
-                    currency: "USD",
-                    amounts: [10, 20,30, 40],
-                    "amountLimits": {
-                        "minimum": 10,
-                        "maximum": 5000
-                    },
-                };
-                api.depositRequests.create({
-                    data: requestDepositData,
-                }).then(function (deposit){
-                    response.token = exchangeToken.fields.token;
-                    response.depositRequestId = deposit.fields.id;
-                    res.send(response);
-                });
-            });
-    });
-
-
-});
-
 app.post('/deposit-usd', (req, res) => {
-    const customerId = 'cus_01J72X65QDGA4DJ7FXSSXMRYRR';
+    const customerId = 'test-customer';
     const response = {};
     const data = {
         mode: "passwordless",
@@ -224,7 +69,7 @@ app.post('/deposit-usd', (req, res) => {
                     websiteId: REBILLY_WEBSITE_ID,
                     customerId,
                     currency: "USD",
-					strategyId: "dep_str_01JAZR9M6QEB792433FNEQKCTV",
+					//strategyId: "dep_str_01JAZR9M6QEB792433FNEQKCTV",
                 };
                 api.depositRequests.create({
                     data: requestDepositData,
@@ -240,7 +85,7 @@ app.post('/deposit-usd', (req, res) => {
 });
 
 app.post('/deposit-cad', (req, res) => {
-    const customerId = 'cus_01J72X65QDGA4DJ7FXSSXMRYRR';
+    const customerId = 'test-customer';
     const response = {};
     const data = {
         mode: "passwordless",
@@ -294,7 +139,7 @@ app.post('/deposit-cad', (req, res) => {
                     websiteId: REBILLY_WEBSITE_ID,
                     customerId,
                     currency: "CAD",
-					strategyId: "dep_str_01JAZRCMXF0K3GH8NYGYYK21TJ",
+					//strategyId: "dep_str_01JAZRCMXF0K3GH8NYGYYK21TJ",
                 };
                 api.depositRequests.create({
                     data: requestDepositData,
